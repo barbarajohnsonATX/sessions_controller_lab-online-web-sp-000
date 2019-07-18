12564709fd99b0fc199343172ad7384328caf211
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-18 00:04:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
     end
  
     def create
-        if params[:name].blank?
-          redirect_to '/login'
-        end 
-        session[:name] = params[:name]
+      if params[:name].present?
+          session[:name] = params[:name]
         redirect_to '/'
+      else 
+        redirect_to '/login'
+        
     end
     
 end
